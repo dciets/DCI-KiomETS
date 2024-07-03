c499884746000f4c1f2ff89fda93fac5a82195c1
Linden Brochu <lindenb477@hotmail.com>
2024-07-02 20:00:33 -0400

    Add a command to admin client

--- clients/client-py/admin.py
+++ clients/client-py/admin.py
@@ -9,11 +9,16 @@ async def admin():
     try:
         client = TcpClient()
         await client.init(env.HOST, env.SUPER_ADMIN_PORT)
+        client2 = TcpClient()
+        await client2.init(env.HOST, 10001)
+        client2.write('id-assign')
+        r = await client2.read()
+        print(r)
         client.write("admin-create " +
                      base64.b64encode(bytes("test", "utf-8")).decode("utf-8") + " " +
                      base64.b64encode(bytes(env.ID, "utf-8")).decode("utf-8"))
         input("Admin create done")
-        client.write("start")
+        client.write("start 0")
     except Exception as e:
         print(e)
 
