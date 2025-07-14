b84f3963811890b1eeaf64ecfe4c16963d4e4b74
Linden Brochu <lindenb477@hotmail.com>
2025-07-14 19:44:04 -0400

    Ajout traduction

--- webserver/frontend/src/app/home/home.component.ts
+++ webserver/frontend/src/app/home/home.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  language: 'fr'|'en';
+  constructor() {
+    this.language = (localStorage.getItem("language") ?? "fr") as 'fr' | 'en';
+  }
 
-  onClick(): void {
-    alert('Button clicked');
+  changeLanguage(language: 'fr'|'en') {
+    this.language = language;
+    localStorage.setItem("language", language);
   }
 
   ngOnInit(): void {}
